3a3c0e01ccb4b40e4ba25ecd7a384629d8820896
ValeriyaReutova <93435168+ValeriyaReutova@users.noreply.github.com>
2021-12-22 19:44:54 +0300

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,19 +1,33 @@
-module.exports = new function(){
-    const data = {};
-    let increment = 0;
-    this.create = dt =>{
-        dt.Id = increment++;
+const { readFileSync, stat, writeFileSync, writeFile } = require('fs')
+
+module.exports = new function () {
+    const fileName = "./data.json";
+    let inc = 0
+    let data = {}
+    this.create = dt => {
+        dt.Id = inc++;
         data[dt.Id] = dt;
-        console.log(data)
-        return dt;
+        writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
+        return dt
     }
-    this.getAll = ()=>{
-        return Object.value(data)
+    this.getAll = () => {
+        return Object.values(data);
     }
-    this.get = id =>  data[id]
+    this.get = id => data[id];
     this.update = dt => {
         data[dt.Id] = dt;
+        writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
         return dt;
     }
-    this.delete = id => delete data[id]
+    this.delete = id => {
+        delete data[id];
+        writeFile(fileName, JSON.stringify(data), err => {if (err) console.error(err);});
+    }
+
+    stat(fileName, (err, stats) => {
+        if (err && err.code === "ENOENT") {
+            writeFileSync(fileName, {});
+        }
+        data = JSON.parse(readFileSync(fileName, {encoding:"UTF-8"}));
+    })
 }
