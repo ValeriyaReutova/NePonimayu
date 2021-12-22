62d85cabe9fb570ed4a95b218ea551b38c7570de
ValeriyaReutova <93435168+ValeriyaReutova@users.noreply.github.com>
2021-12-22 19:41:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-const http = require("http"); 
-    crud = require("./crud");
+const http = require("http"),
+    crud = require("./crud"),
+    statics = require("node-static");
 
-const echo = (req, content) => {
-    res.end(JSON.stringify(content)); 
+const staticFileDir = new statics.Server("./public");
+
+const echo = (res, content) => {
+    res.end(JSON.stringify(content));
 }
 
-const student = (req,res)=>{
-    res.writeHead(200,{"Content-type":"application/json"})
+const student = (req, res) => {
+    res.writeHead(200,{"Content-type": "application/json"});
+
     const url = req.url.substring(1).split("/");
-    switch(req.method){
+
+    switch (req.method) {
         case "GET":
-            if (url.length>1)
-                echo(res,crud.get(url[1]));
+            if (url.length > 1)
+                echo (res, crud.get(url[1]))
             else
-                echo(res,crud.getAll());
+                echo (res, crud.getAll())
             break;
         case "POST":
-            getAsyncData(req, data=>{
-                echo(res,crud.create(JSON.parse(data)));
-            });
+            getAsyncData (req, data => {
+                echo(res, crud.create(JSON.parse(data)))
+            })
             break;
         case "PUT":
-            getAsyncData(req, data=> {
-                echo(res,crud.update(JSON.parse(data)));
-            });
+            getAsyncData (req, data => {
+                echo(res, crud.update(JSON.parse(data)))
+            })
             break;
         case "DELETE":
-            if (url.length>1)
-                echo(res,crud.delete(url[1]));
+            if(url.length > 1)
+                echo(res, crud.delete(url[1]))
             else
-                echo(res,({error:"Не передан id"}));
+                echo(res,{error:"Не передан id"})
             break;
+        default: echo(res,{error:"500"})
     }
 }
-const getAsyncData =(req,callback)=>{
-    let data = "";
-    req.on("data", chunk=>{data+=chunk;});
-    req.on("end", ()=>{callback(data);});
-}
-const handler = function(req, res){
-    const url = req.url.substring(1).split("/");
-    switch(url[0]){
-        case "student":
-            student(req,res);
-            return;
-        case "":
-            res.end("index");
-            return;
 
-    }
-    console.log("AFTER SWITCH");
-    console.log(req.url);
-    console.log(req.method);
-    res.end("echo");
+const getAsyncData = (req, callback) => {
+    let data = "";
+    req.on("data", chunk => {data += chunk})
+    req.on("end", () => {callback(data)})
 }
-http.createServer(handler).listen(8090()=>{
-    console.log("run");
+
+const handler = function (req, res) {
+    const url = req.url.substring(1).split("/")
+    switch (url[0]) {
+        case "student":
+            student(req, res);
+            return
+    }
+    staticFileDir.serve(req, res);
+}
+
+http.createServer(handler).listen(8108, () => {
+    console.log("run")
 })
